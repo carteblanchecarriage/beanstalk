f1ab0d2cac1ec5383b973b98952e8a5ed83d6a6b
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2024-06-29 18:50:43 -0400

    comments

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,13 +4,78 @@ import ReactMarkdown from 'react-markdown';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Link from 'next/link';
-
+import supabase from '../../utils/supabase';
 import { getPosts } from '../api/getHeadless';
 import Markdown from 'react-markdown';
 
 export default function Post({ post }) {
+  const [isClient, setIsClient] = useState(false);
+  const [user, setUser] = useState('');
+  const [comment, setComment] = useState('');
+  const [hexColor, setHexColor] = useState('');
+  const [allComments, setAllComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const router = useRouter();
   const { slug } = router.query;
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const fetchComments = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('comments')
+        .select('id, user, comment, color')
+        .eq('slug', slug);
+
+      if (error) {
+        throw error;
+      }
+      console.log(data);
+
+      setAllComments(data || []);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchComments();
+    console.log(allComments);
+  }, [allComments]);
+
+  const generateRandomColor = () => {
+    const randomColor = Math.floor(Math.random() * 0xffffff).toString(16);
+    setHexColor(`#${randomColor}`);
+  };
+
+  const uploadComment = async () => {
+    setIsLoading(true);
+    if (!user || !comment || !hexColor) {
+      setIsLoading(false);
+      return;
+    }
+    try {
+      const { data, error } = await supabase
+        .from('comments')
+        .insert([{ slug: slug, user: user, comment: comment, color: hexColor }])
+        .select();
+
+      if (error) {
+        setIsLoading(false);
+        throw error;
+      }
+      setUser('');
+      setComment('');
+      setHexColor('');
+      fetchComments();
+      setIsLoading(false);
+      return data;
+    } catch (error) {
+      console.error('Error inserting comment:', error);
+      return null;
+    }
+  };
 
   return (
     <>
@@ -35,13 +100,15 @@ export default function Post({ post }) {
             img: ({ node, ...props }) => {
               const { src, alt } = props;
               return (
-                <div className='flex justify-center mb-4'>
+                <div className='mb-4'>
                   {' '}
+                  {/* Ensure this div is not inside a <p> */}
                   <Image
                     src={src}
                     alt={alt}
                     width={500} // Adjust width and height as needed
                     height={500}
+                    loading='lazy'
                   />
                 </div>
               );
@@ -50,6 +117,62 @@ export default function Post({ post }) {
         >
           {post.postContent.markdown}
         </Markdown>
+      </div>
+      <div className='mx-auto items-center flex flex-col w-full'>
+        {hexColor ? (
+          <div className='w-3/4'>
+            <span
+              className='inline-block w-6 h-6 rounded-full bg-gray-300 dark:bg-gray-600 mr-3'
+              style={{ backgroundColor: hexColor }}
+            ></span>
+            <p>name</p>
+            <textarea
+              className='bg-gray-100 inline-block mb-4 w-full p-2'
+              onChange={(event) => setUser(event.target.value)}
+            ></textarea>
+            <p>comment</p>
+            <textarea
+              className='bg-gray-100 block mb-4 w-full h-24 p-2'
+              onChange={(event) => setComment(event.target.value)}
+            ></textarea>
+            <button
+              className='bg-primaryColor hover:bg-gray-300 text-white font-bold py-2 px-4 rounded mb-4'
+              type='button'
+              onClick={uploadComment}
+            >
+              {isLoading ? (
+                <p className='bg-primaryColor'>Uploading</p>
+              ) : (
+                'Upload Comment'
+              )}
+            </button>
+          </div>
+        ) : (
+          <button
+            className='bg-primaryColor hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4'
+            type='button'
+            onClick={generateRandomColor}
+          >
+            Add Comment
+          </button>
+        )}
+        <div className='w-3/4 mx-12'>
+          {allComments.map((comment) => (
+            <div key={comment.id} className='flex flex-col mb-6'>
+              <div>
+                <span
+                  className='inline-block w-6 h-6 rounded-full bg-gray-300 dark:bg-gray-600 mr-3'
+                  style={{ backgroundColor: comment?.color }}
+                ></span>
+                <span className='text-gray-900 font-semibold'>
+                  {comment?.user}
+                </span>
+              </div>
+              <p className='text-gray-600 overflow-wrap'>{comment?.comment}</p>
+            </div>
+          ))}
+        </div>
+        <div className='mt-4'></div>
       </div>
     </>
   );
@@ -64,7 +187,7 @@ export async function getStaticProps(context) {
 
   const post = posts.find((item) => item.slug === slug);
 
-  return { props: { post }, revalidate: 10 };
+  return { props: { post }, revalidate: 60 };
 }
 
 export async function getStaticPaths() {
@@ -74,7 +197,6 @@ export async function getStaticPaths() {
     const paths = posts.map((post) => ({
       params: { slug: post.slug },
     }));
-    console.log(paths);
 
     return { paths, fallback: 'blocking' };
   } catch (error) {
